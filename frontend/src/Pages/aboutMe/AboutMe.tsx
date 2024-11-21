31c3bfd8a47ff4aa77c0c9742c2742b665c78569
wilfy7 <wilfy7am@gmail.com>
2024-11-21 09:53:17 +0100

    update

--- frontend/src/Pages/aboutMe/AboutMe.tsx
+++ frontend/src/Pages/aboutMe/AboutMe.tsx
@@ -9,15 +9,27 @@ const AboutMe = () => {
          <h1 className="about-h1">Wilfred Ameyaw-Akumfi- <br/> Software Engineer & Digital Marketing Expert</h1>
          <p className="about-p">Transforming visions into reality with dependable technology and innovative software craftsmanship</p>
          <div className="about-btn">
-          <button className="btn">
-            <p className="btn-prim">Create Something Extraordinary With Us</p>
+          <button className="btn-m btn-primary">
+          <a  
+                href="https://calendly.com/wilfy7am/book-a-meeting-with-wilfred" 
+                className="btn-primary">
+                  Create Something Extraordinary With Us
+          </a>
           </button>
-          <button>
-            <p className="btn-prim2">Learn more about how we hoist your brand </p>
+         
+          <button className="btn-m btn-secondary">
+            <a  
+               href="https://calendly.com/wilfy7am/book-a-meeting-with-wilfred" 
+               className="anch">
+                 See How I Can Help You Hoist Your Brand 
+            </a>
           </button>
+         
         </div>
         </div>
-        
+        <div className="about-img-sec">
+          <img className="about-img" src="images/wilfy.png" alt="wilfy" />
+        </div>
       </div>
     </section >
   )
