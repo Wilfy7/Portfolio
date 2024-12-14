f9b25d73a82347dc9d52f3abc7473128cf48e042
wilfy7 <wilfy7am@gmail.com>
2024-12-14 21:56:52 +0100

    update

--- frontend/src/Pages/home/Home.tsx
+++ frontend/src/Pages/home/Home.tsx
@@ -60,6 +60,105 @@ const Home = () => {
         <span className="let-section">Let us Bring Your Projects to Life Together</span>
       </div>
 
+
+       <div className="attributes-section">
+         <div className="attribute-items">
+         <svg   viewBox="0 0 16 16" className="fi">
+          <path d="M11.251.068a.5.5 0 0 1 .227.58L9.677 6.5H13a.5.5 0 0 1 .364.843l-8 8.5a.5.5 0 0 1-.842-.49L6.323 9.5H3a.5.5 0 0 1-.364-.843l8-8.5a.5.5 0 0 1 .615-.09z"/>
+         </svg>          
+           <h5 className="attribute-header">Dynamic Solutions</h5>
+          <span className="span-txt">
+            Creatingh hshsdjsbjksdjbbhbfdb  ns xbs  sc sb  <br />cbs cs
+            hdjnwd hwndu3nu3fbu3  
+          </span>
+         </div>
+
+         <div className="attribute-items">
+         <svg  className="fi" viewBox="0 0 512 512" id="icons" xmlns="http://www.w3.org/2000/svg">
+          <path d="M208,512a24.84,24.84,0,0,1-23.34-16l-39.84-103.6a16.06,16.06,0,0,0-9.19-9.19L32,343.34a25,25,0,0,1,0-46.68l103.6-39.84a16.06,16.06,0,0,0,9.19-9.19L184.66,144a25,25,0,0,1,46.68,0l39.84,103.6a16.06,16.06,0,0,0,9.19,9.19l103,39.63A25.49,25.49,0,0,1,400,320.52a24.82,24.82,0,0,1-16,22.82l-103.6,39.84a16.06,16.06,0,0,0-9.19,9.19L231.34,496A24.84,24.84,0,0,1,208,512Zm66.85-254.84h0Z"/><path d="M88,176a14.67,14.67,0,0,1-13.69-9.4L57.45,122.76a7.28,7.28,0,0,0-4.21-4.21L9.4,101.69a14.67,14.67,0,0,1,0-27.38L53.24,57.45a7.31,7.31,0,0,0,4.21-4.21L74.16,9.79A15,15,0,0,1,86.23.11,14.67,14.67,0,0,1,101.69,9.4l16.86,43.84a7.31,7.31,0,0,0,4.21,4.21L166.6,74.31a14.67,14.67,0,0,1,0,27.38l-43.84,16.86a7.28,7.28,0,0,0-4.21,4.21L101.69,166.6A14.67,14.67,0,0,1,88,176Z"/><path d="M400,256a16,16,0,0,1-14.93-10.26l-22.84-59.37a8,8,0,0,0-4.6-4.6l-59.37-22.84a16,16,0,0,1,0-29.86l59.37-22.84a8,8,0,0,0,4.6-4.6L384.9,42.68a16.45,16.45,0,0,1,13.17-10.57,16,16,0,0,1,16.86,10.15l22.84,59.37a8,8,0,0,0,4.6,4.6l59.37,22.84a16,16,0,0,1,0,29.86l-59.37,22.84a8,8,0,0,0-4.6,4.6l-22.84,59.37A16,16,0,0,1,400,256Z"/>
+         </svg>         
+           <h5 className="attribute-header">Dynamic Solutions</h5>
+          <span className="span-txt">
+            Creatingh hshsdjsbjksdjbbhbfdb  ns xbs  sc sb  <br />cbs cs
+            hdjnwd hwndu3nu3fbu3  
+          </span>
+         </div>
+
+         <div className="attribute-items">
+         <svg className="fi" version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 489.3 489.3">
+           <g>
+	           <path d="M95.55,489.3h298.2c21.4,0,38.8-17.4,38.8-38.8V236.8c0-21.4-17.4-38.8-38.8-38.8h-1v-51.1c0-81-65.9-146.9-146.9-146.9
+		          S98.95,65.9,98.95,146.9V198h-3.4c-21.4,0-38.8,17.4-38.8,38.8v213.7C56.75,472,74.15,489.3,95.55,489.3z M262.25,349.3v14.4
+		          c0,1.7-0.7,3.3-1.9,4.5l-13.1,13.1c-0.4,0.4-0.4,1,0,1.4l9.8,9.8c0.6,0.6,0.6,1.5,0,2.1l-10.9,10.9c-0.4,0.4-0.4,1,0,1.4l9.2,9.2
+		          c0.9,0.9,1.4,2.1,1.4,3.4v3.6l-12.8,12.5c-1.1,1.1-2.8,1.1-4,0.1l-11-9.5c-1.8-1.6-2.9-3.9-2.9-6.4v-71l0,0
+		          c-10.7-6.3-17.9-18-17.9-31.3c0-20.1,16.3-36.3,36.3-36.3s36.3,16.3,36.3,36.3C280.95,331.1,273.45,343.1,262.25,349.3z
+		          M163.25,147c0-45.6,37.1-82.6,82.6-82.6c45.6,0,82.6,37.1,82.6,82.6v51.1h-165.2V147z"/>
+           </g>
+         </svg>          
+           <h5 className="attribute-header">Dynamic Solutions</h5>
+          <span className="span-txt">
+            Creatingh hshsdjsbjksdjbbhbfdb  ns xbs  sc sb  <br />cbs cs
+            hdjnwd hwndu3nu3fbu3  
+          </span>
+         </div>
+
+         <div className="attribute-items">
+         <svg 
+            className="fi" version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" 
+	          viewBox="0 0 487.403 487.403"
+	         >
+           <g>
+	           <path d="M485.485,147.168c-8.912-51.251-51.034-91.912-102.524-99.306c-57.862-8.302-109.416,23.824-130.581,72.271
+		              c-1.689,3.871-5.16,5.933-8.678,5.933c-3.518,0-6.989-2.062-8.678-5.933c-21.165-48.446-72.718-80.572-130.581-72.271
+		              C52.952,55.256,10.83,95.917,1.918,147.168C-5.27,188.5,8.264,226.919,33.846,253.754c5.561,6.711,14.008,16.156,20.27,23.298
+		              c33.494,38.2,105.859,107.919,152.262,149.677c8.863,7.988,21.044,13.609,34.087,14.074c1.617,0.059,4.855,0.059,6.473,0
+		              c13.043-0.465,25.226-6.086,34.087-14.074c46.403-41.758,118.77-111.477,152.262-149.677c6.263-7.142,14.709-16.587,20.271-23.298
+		              C479.14,226.919,492.675,188.5,485.485,147.168z"
+              />
+           </g>
+          </svg>        
+           <h5 className="attribute-header">Dynamic Solutions</h5>
+          <span className="span-txt">
+            Creatingh hshsdjsbjksdjbbhbfdb  ns xbs  sc sb  <br />cbs cs
+            hdjnwd hwndu3nu3fbu3  
+          </span>
+         </div>
+
+         <div className="attribute-items">
+         <svg  className="fi"
+             version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 472.618 472.618" 
+           >
+            <g>
+	           <g>
+		            <path 
+                  d="M380.525,337.291c-135.427,0-245.302-109.773-245.302-245.302c0-32.502,6.338-63.575,17.991-91.988
+			            C63.372,36.286,0,124.39,0,227.315c0,135.427,109.875,245.302,245.302,245.302c102.923,0,191.029-63.472,227.316-153.315
+			            C444.201,330.954,413.129,337.291,380.525,337.291z"
+                />
+	           </g>
+            </g>
+          </svg>         
+           <h5 className="attribute-header">Dynamic Solutions</h5>
+          <span className="span-txt">
+            Creatingh hshsdjsbjksdjbbhbfdb  ns xbs  sc sb  <br />cbs cs
+            hdjnwd hwndu3nu3fbu3  
+          </span>
+         </div>
+
+        <div className="attribute-items">
+          <svg className="fi" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+             <g>
+                 <path d="M12 0a1 1 0 0 1 1 1v4a1 1 0 1 1-2 0V1a1 1 0 0 1 1-1ZM4.929 3.515a1 1 0 0 0-1.414 1.414l2.828 2.828a1 1 0 0 0 1.414-1.414L4.93 3.515ZM1 11a1 1 0 1 0 0 2h4a1 1 0 1 0 0-2H1ZM18 12a1 1 0 0 1 1-1h4a1 1 0 1 1 0 2h-4a1 1 0 0 1-1-1ZM17.657 16.243a1 1 0 0 0-1.414 1.414l2.828 2.828a1 1 0 1 0 1.414-1.414l-2.828-2.828ZM7.757 17.657a1 1 0 1 0-1.414-1.414L3.515 19.07a1 1 0 1 0 1.414 1.414l2.828-2.828ZM20.485 4.929a1 1 0 0 0-1.414-1.414l-2.828 2.828a1 1 0 1 0 1.414 1.414l2.828-2.828ZM13 19a1 1 0 1 0-2 0v4a1 1 0 1 0 2 0v-4ZM12 7a5 5 0 1 0 0 10 5 5 0 0 0 0-10Z"/>
+             </g>
+          </svg>         
+           <h5 className="attribute-header">Dynamic Solutions</h5>
+          <span className="span-txt">
+            Creatingh hshsdjsbjksdjbbhbfdb  ns xbs  sc sb  <br />cbs cs
+            hdjnwd hwndu3nu3fbu3  
+          </span>
+        </div>
+       </div>
+
       <div className="img2-container">
         <div>
          <img className="img2-items" src="images/Inov2.webp" alt="Inov" />
