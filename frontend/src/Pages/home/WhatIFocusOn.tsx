3621367d2cf7b0453c452879abe5b31031347361
wilfy7 <wilfy7am@gmail.com>
2025-04-09 00:47:31 +0200

    update

--- frontend/src/Pages/home/WhatIFocusOn.tsx
+++ frontend/src/Pages/home/WhatIFocusOn.tsx
@@ -3,9 +3,21 @@ import "./HomeStyles.scss"
 
 const WhatIFocusOn = () => {
   return (
-    <div className="what-i-focus-on-section">
+    <div className="what-i-focus-on-container">
       <div className="my-focus">
      <span>What I Focus On</span>
+     </div>
+
+     <div className="my-focus-content">
+      <span>
+        I am an AWS cloud practitioner and a software developer. 
+        I have the ability to build extensible applications and 
+        I design robust cloud infracstructure that ensures high 
+        availability for modern applications. 
+        This keeps me curious and make me evolve constantly.
+        From contributing to a larger vision to driving a project solo, 
+        I bring both versatility and impact to the table
+        </span>
      </div>
     </div>
   )
